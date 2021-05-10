4c9ff3ca38ab5887d9c2c5b07e4133ada524f53b
Muhammet Balıkçı <muhammetbalikci0@outlook.com>
2021-05-10 05:20:55 +0300

    MyDictionary(Just Add)

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] key;
+        V[] value;
+
+        public MyDictionary()
+        {
+            key = new K[0];
+            value = new V[0];
+        }
+
+        public void Add(K k, V v)
+        {
+            K[] tempKey = key;
+            V[] tempValue = value;
+            key = new K[key.Length + 1];
+            value = new V[value.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                key[i] = tempKey[i];
+                value[i] = tempValue[i];
+            }
+            key[key.Length - 1] = k;
+            value[value.Length - 1] = v;
+           
+        }
+        public void print()
+        {
+            for (int i = 0; i < key.Length; i++)
+            {
+                Console.WriteLine(key[i] + ", " + value[i]);
+                
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> md = new MyDictionary<int,string>();
+            md.Add(111,"Muhammet");
+            md.print();
+
+        }
+    }
+}
